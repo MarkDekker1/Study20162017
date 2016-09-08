cea1ff57dbcf7814b8831d7346cc8dd351370bf2
Mark <m.m.dekker@outlook.com>
2016-09-08 09:37:26 +0200

    Home

--- MAIO/Ex1a.py
+++ MAIO/Ex1a.py
@@ -8,11 +8,11 @@ matplotlib.style.use('ggplot')
 
 #Constants
 alpha1=0.75
-alpha2=-0.3
+alpha2=0.0
 length=100
 
 #Create AR(2) data
-ARdata=np.zeros(length)+1.
+ARdata=np.zeros(length)+2.
 for i in range(2,length):
     ARdata[i]=alpha1*ARdata[i-1]+alpha2*ARdata[i-2]
 
@@ -25,73 +25,49 @@ for k in range(0,length):
         ACvector[i]=(ARdata[i]-Mean)*(ARdata[i+k]-Mean)
     
     ACfunction[k]=sum(ACvector)/np.var(ARdata)/(length-k)
+ACfunction=ACfunction[1:]
 
-#%%Create Partial Autocorrelation function
-#http://stats.stackexchange.com/questions/129052/acf-and-pacf-formula
-L0=ARdata[0:(length-3)]
-L1=ARdata[1:(length-2)]
-L2=ARdata[2:(length-1)]
-
-rho12=(length*np.dot(L0,L1)-sum(L0)*sum(L1))/np.sqrt((length*np.dot(L0,L0)-(sum(L1))**2)*(length*np.dot(L0,L0)-(sum(L1))**2))
-rho23=(length*np.dot(L1,L2)-sum(L1)*sum(L2))/np.sqrt((length*np.dot(L1,L1)-(sum(L2))**2)*(length*np.dot(L1,L1)-(sum(L2))**2))
-rho13=(length*np.dot(L0,L2)-sum(L0)*sum(L2))/np.sqrt((length*np.dot(L0,L0)-(sum(L2))**2)*(length*np.dot(L0,L0)-(sum(L2))**2))
-
-rho13_2=(rho13-rho12*rho23)/np.sqrt((1-rho12**2)*(1-rho13**2))
-
-#%% say up to k=4
-Phivector=np.zeros(20)
-for k in range(3,20):
-    
-    Kmax=k
-    Matrix=np.zeros(shape=(Kmax,Kmax))
-    for i in range(0,Kmax):
-        for j in range(0,Kmax):
-            for m in range(0,Kmax):
-                if np.abs(i-j)==m:
-                    Matrix[i,j]=ARdata[m]
-    
-    InvMatrix=inv(Matrix)
-    Kvec=np.dot(InvMatrix,ARdata[1:(Kmax+1)])
-    Phivector[k]= Kvec[k-1]
-
-
-#%%
+#%%https://www.empiwifo.uni-freiburg.de/lehre-teaching-1/winter-term/dateien-financial-data-analysis/handout-pacf.pdf
 SIZE=20
 PhiMatrix=np.zeros(shape=(SIZE,SIZE))
 
 PhiMatrix[0,0]=ACfunction[0]
 PhiMatrix[1,1]=(ACfunction[1]-(ACfunction[0])**2)/(1-(ACfunction[0])**2)
-for k in range(1,SIZE):
+PhiMatrix[1,0]=PhiMatrix[0,0]-PhiMatrix[1,1]*PhiMatrix[0,0]#(ACfunction[0]-ACfunction[1]*ACfunction[0])/(1-PhiMatrix[1,1]*ACfunction[0])
+PhiMatrix[2,2]=(ACfunction[2]-PhiMatrix[1,0]*ACfunction[1]-PhiMatrix[1,1]*ACfunction[0])/(1-PhiMatrix[1,0]*ACfunction[1]-PhiMatrix[1,1]*ACfunction[0])
 
-    for j in range(0,k):
-        print(j)
-        PhiMatrix[k,j]=PhiMatrix[k-1,j]-PhiMatrix[k,k]*PhiMatrix[k-1,k-j]
-        
-    if k>=2:
+for k in range(2,SIZE):
+    
+    if k>=3:
         Sumvec1=[]
-        Sum1vec2=[] 
-        Sum2vec2=[]  
-          
-        for j in range(0,k-1):
-            Sumvec1.append(PhiMatrix[k-1,j])
-            Sum1vec2.append(ACfunction[k-j])
-            Sum2vec2.append(ACfunction[j])
-            
-        PhiMatrix[k,k]=(ACfunction[k]-np.dot(Sumvec1,Sum1vec2))/(1-np.dot(Sumvec1,Sum2vec2))
-
-plt.plot(np.diag(PhiMatrix))
-
+        Sumvec2=[]
+        for r in range(0,k):
+            Sumvec1.append(PhiMatrix[k-1,r])
+            Sumvec2.append(ACfunction[k-r])    
+        PhiMatrix[k,k]=(ACfunction[k]-np.dot(Sumvec1,Sumvec2))/(1-np.dot(Sumvec1,Sumvec2))
+    
+    for j in range(0,k):
+        PhiMatrix[k,j]=PhiMatrix[k-1,j]-PhiMatrix[k,k]*PhiMatrix[k-1,k-j-1]
+        
 #%%Plot data
 plt.figure(num=None, figsize=(5,4),dpi=150, facecolor='w', edgecolor='k')
 plt.plot(ARdata, 'k-',linewidth=3)
-plt.ylabel('AR(2) data',fontsize=20)
+plt.ylabel('Data',fontsize=20)
 plt.xlabel('Time',fontsize=20)
 plt.tick_params(axis='both', which='major', labelsize=20)
 
 #Plot autocorrelation function
 plt.figure(num=None, figsize=(5,4),dpi=150, facecolor='w', edgecolor='k')
 plt.plot(ACfunction, 'k-',linewidth=3)
-plt.ylabel('Rho',fontsize=20)
+plt.ylabel('ACF',fontsize=20)
 plt.xlabel('Time lag',fontsize=20)
-plt.xlim([0,len(ARdata)/3])
+plt.xlim([0,SIZE])
+plt.tick_params(axis='both', which='major', labelsize=20)
+
+#Plot partial autocorrelation function
+plt.figure(num=None, figsize=(5,4),dpi=150, facecolor='w', edgecolor='k')
+plt.plot(np.diag(PhiMatrix), 'k-',linewidth=3)
+plt.ylabel('PACF',fontsize=20)
+plt.xlabel('Time lag',fontsize=20)
+plt.xlim([0,SIZE])
 plt.tick_params(axis='both', which='major', labelsize=20)
