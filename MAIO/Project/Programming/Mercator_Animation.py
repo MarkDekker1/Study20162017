02821e1aa8c4f870c4bba0fbf6c0176cbea9ce26
Mark Dekker <Mark Dekker>
2016-10-07 10:49:48 +0200

    laptop

--- MAIO/Project/Programming/Mercator_Animation.py
+++ MAIO/Project/Programming/Mercator_Animation.py
@@ -11,12 +11,12 @@ import math
 import netCDF4 as netcdf
 from copy import copy
 import matplotlib.pyplot as plt
-#from mpl_toolkits.basemap import Basemap
+from mpl_toolkits.basemap import Basemap
 
 # ------------------------------------------------------
 # Read data
 # ------------------------------------------------------
-file='C:/Users/Mark Dekker/OneDrive/Data/MAIO_Project/Mercator_2h_august_18_september_7.nc'
+file='C:\Users\Mark\Documents\Studie\Data\MAIO_Project/Mercator_2h_august_18_september_7.nc'
 DataSet = netcdf.Dataset(file, mode='r')
 
 date	= DataSet.variables['time_counter'][:] 	#Hours passed since 1-1-1950, 00:00:00
@@ -49,7 +49,6 @@ for i in range(0,len(temp)):
     U2.append(dummyu)
     V2.append(dummyv)
     
-#%%
 # ------------------------------------------------------
 # Remove bad data (on the islands)
 # ------------------------------------------------------
@@ -61,22 +60,39 @@ for i in range(0,len(U3)):
     U3[i][maxu]='nan'
     V3[i][maxv]='nan'
 
-
 #%%
+# ------------------------------------------------------
+# Start Basemap
+# ------------------------------------------------------
+
+m = Basemap(
+projection = 'merc',
+llcrnrlat=17, urcrnrlat=18,
+llcrnrlon=-65, urcrnrlon=-60,
+resolution='i', area_thresh=0.01
+) 
+			
+m.drawcoastlines(linewidth=0.2)
+m.fillcontinents(color='#cc9966')
+
+x,y=m(Lon_bath,Lat_bath)
+c=plt.contour(x,y,-Depth,levels=[10,30,100,1000],cmap=plt.cm.Greys,norm=colors.LogNorm(vmin=3,vmax=10000))
+par = m.drawparallels(np.arange(-80,81,0.5),labels=[1,0,0,0])
+mer = m.drawmeridians(np.arange(-180,180,1),labels=[0,0,0,1])
+
 # ------------------------------------------------------
 # Start animation
 # ------------------------------------------------------
 
 from matplotlib import animation
 
-fig = plt.figure()
 x=0
 t=2
 U, V = U3[0], V3[0]
 
 def init():
     b=temp2[0]
-    imobj.set_data(b[::-1])
+    imobj.set_data(b)
     time_text.set_text('time = 0:00')
     return imobj,time_text
 
@@ -89,34 +105,39 @@ def animate(self):
     t += 2
     t = np.mod(t,24)
     a=temp2[x]
-    imobj.set_data(a[::-1])
+    imobj.set_data(a)
     au=U3[x]
     av=V3[x]
     
     time_text.set_text('time = %.0f:00' % t )
     
     Quivers.set_UVC(au,av)
-    qk = plt.quiverkey(Quivers, 0.5, 1.03, 0.5, r'$0.5 \frac{m}{s}$', labelpos='W',
-                       fontproperties={'weight': 'bold'})
+    #qk = plt.quiverkey(Quivers, 0.5, 1.03, 0.5, r'$0.5 \frac{m}{s}$', labelpos='W',
+    #                   fontproperties={'weight': 'bold'})
     
-    return imobj,time_text,Quivers,qk
+    return imobj,time_text,Quivers#,qk
     
-c=temp2[0]
-data=c[::-1]
+data=temp2[0]
 my_cmap = copy(cm.get_cmap('jet'))
 my_cmap.set_bad('white')
 my_cmap.set_under('white')
-imobj=plt.imshow(data, cmap=my_cmap, animated=True, extent=[-65,-60,17,18],aspect=2)
+imobj=m.imshow(data, cmap=my_cmap, animated=True, extent=[-65,-60,17,18],aspect=2,zorder=-1)
 
-plt.colorbar(orientation='horizontal')
+a=plt.colorbar(imobj,orientation='horizontal')#,size='5%')#,pad='2%')
 plt.clim(301.5,303.5)
-time_text = plt.text(-65,18, '', zorder=10)
+time_text = plt.text(-64,17.5, '', zorder=10)
 
-Quivers=plt.quiver(lon2,lat2,U,V,units='inches',scale=0.3,pivot='tip',width=0.015)#units='x',pivot='tip',width=0.008,scale=1 / 0.5)
+x2,y2=meshgrid(lon2,lat2)
+xx,yy=m(x2,y2)
+#up,vp,xx,yy=\
+#m.transform_vector(U,V,lon2,lat2,31,31,returnxy=True,masked=True)
+
+Quivers=m.quiver(xx,yy,U,V,units='inches',scale=0.6,pivot='tip',width=0.015,zorder=15)#units='x',pivot='tip',width=0.008,scale=1 / 0.5)
 qk = plt.quiverkey(Quivers, 0.5, 1.03, 0.5, r'$0.5 \frac{m}{s}$', labelpos='W',
                    fontproperties={'weight': 'bold'})
+                   
 
-anim = animation.FuncAnimation(fig, animate, init_func=init,
+anim = animation.FuncAnimation(plt.gcf(), animate, init_func=init,
                                frames=range(len(temp2)), interval=500)
 
 plt.show()
